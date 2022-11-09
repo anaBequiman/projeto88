c39b061a76520e4e5589a5c8d23dcc38ce0b9926
Ana <101293323+anaBequiman@users.noreply.github.com>
2022-11-09 17:53:57 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+
+var canvas = new fabric.Canvas('meuCanvas');
+var bolaX = 0;
+var bolaY = 0;
+var buracoX = 1800;
+var buracoY = 900;
+var bolaLargura = 5;
+var bolaAltura = 5;
+var buracoObjeto = " ";
+var bolaObjeto = " ";
+function loadImg() {
+    fabric.Image.fromURL("buraco.png", function (Img) {
+        buracoObjeto = Img;
+        buracoObjeto.scaleToWidth(50);
+        buracoObjeto.scaleToHeight(50);
+        buracoObjeto.set({
+            top:buracoY,
+            left:buracoX
+        });
+        canvas.add(buracoObjeto);
+    });
+    newImage();
+}
+function newImage() {
+    fabric.Image.fromURL("bola.png", function (Img) {
+        bolaObjeto = Img;
+        bolaObjeto.scaleToWidth(50);
+        bolaObjeto.scaleToHeight(50);
+        bolaObjeto.set({
+            top:bolaY,
+            left:bolaX
+        });
+        canvas.add(bolaObjeto);
+    });
+}
+window.addEventListener("keydown", myKeyDown);
+function myKeyDown(e) {
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if((bolaX==buracoX) && (bolaY==buracoY)){
+        canvas.remove(bolaObjeto);
+        document.getElementById("mensagem").innerHTML = "Parabéns, você atingiu o objetivo!!!";
+        document.getElementById("meuCanvas").style.borderColor = "red";
+    }
+    else{
+        if (keyPressed == '38') {
+            cima();
+            console.log("up");
+        }
+        if (keyPressed == '40') {
+            baixo();
+            console.log("down");
+        }
+        if (keyPressed == '37') {
+            esquerda();
+            console.log("left");
+        }
+        if (keyPressed == '39') {
+            direita();
+            console.log("right");
+        }
+    }
+    function cima() {
+        if(bolaY >= 0){
+            bolaY = bolaY - bolaAltura;
+            console.log("Altura da bola = " + bolaAltura);
+            console.log("Quando a tecla direcional cima é precionada, X = " + bolaX + ", Y = "+ bolaY)
+            canvas.remove(bolaObjeto);
+            newImage();
+        }
+    } 
+    function baixo() {
+        if(bolaY <=940){
+            bolaY = bolaY + bolaAltura;
+            console.log("Altura da bola = " + bolaAltura);
+            console.log("Quando a tecla direcional baixo é precionada, X = " + bolaX + ", Y = "+ bolaY);
+            canvas.remove(bolaObjeto);
+            newImage();
+        }
+    }
+    function esquerda() {
+        if (bolaX > 5) {
+            bolaX = bolaX - bolaLargura;
+            console.log("Largura da bola = " + bolaLargura);
+            console.log("Quando a tecla direcional esquerda é precionada, X = " + bolaX + ", Y = " + bolaY);
+            canvas.remove(bolaObjeto);
+            newImage();
+        }
+    }
+    function direita() {
+        if (bolaX <= 1800) {
+            bolaX = bolaX + bolaLargura;
+            console.log("Largura da bola = " + bolaLargura);
+            console.log("Quando a tecla direcional direita é precionada, X = " + bolaX + ", Y = " + bolaY);
+            canvas.remove(bolaObjeto);
+            newImage();
+        }
+    }
+}
